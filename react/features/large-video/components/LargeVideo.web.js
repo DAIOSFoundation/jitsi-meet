1b7bac18beb15130b24b3340f5c79cc5246d06af
서강혁 <rkdgur7007@gmail.com>
2020-08-26 14:53:11 +0900

    [수정] WEBINAR 퍼블리싱 기능 구현

--- react/features/large-video/components/LargeVideo.web.js
+++ react/features/large-video/components/LargeVideo.web.js
@@ -90,7 +90,7 @@ class LargeVideo extends Component<Props> {
                 </div>
                 <div id='etherpad'/>
                 {
-                    this.state.urlValue === 'WEBINAR' ?
+                    this.state.urlValue === 'WEBINAR' || this.state.urlValue === 'WEBINAR2' ?
                         <img src={'images/top_banner.png'} style={{zIndex:100, position:'absolute', left:0, height:'150px', width:'86%'}} width={'100%'}
                              height={'150px'}/>
                              :
