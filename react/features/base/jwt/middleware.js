3acc2e7e0c857d639a797314a9584ffc116d7d64
서강혁 <rkdgur7007@gmail.com>
2020-09-23 16:31:02 +0900

    token test

--- react/features/base/jwt/middleware.js
+++ react/features/base/jwt/middleware.js
@@ -102,7 +102,7 @@ function _setConfigOrLocationURL({ dispatch, getState }, next, action) {
     const { locationURL } = getState()['features/base/connection'];
 
     dispatch(
-        setJWT('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250ZXh0Ijp7InVzZXIiOnsiYXZhdGFyIjoiaHR0cHM6Ly9yb2JvaGFzaC5vcmcvam9obi1kb2UiLCJuYW1lIjoiSm9obiBEb2UiLCJlbWFpbCI6Impkb2VAZXhhbXBsZS5jb20ifX0sImF1ZCI6ImR2aXNpb24iLCJpc3MiOiJkdmlzaW9uIiwic3ViIjoiZHZpc2lvbi5kYWlvcy5uZXQiLCJyb29tIjoiKiJ9.X4AvlNrkjK4RBXGdORrj_OtCZK1o6UKdaWZU-gO1yic');
+        setJWT('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250ZXh0Ijp7InVzZXIiOnsiYXZhdGFyIjoiaHR0cHM6Ly9yb2JvaGFzaC5vcmcvam9obi1kb2UiLCJuYW1lIjoiSm9obiBEb2UiLCJlbWFpbCI6Impkb2VAZXhhbXBsZS5jb20ifX0sImF1ZCI6ImR2aXNpb24iLCJpc3MiOiJkdmlzaW9uIiwic3ViIjoiZHZpc2lvbi5kYWlvcy5uZXQiLCJyb29tIjoiKiJ9.X4AvlNrkjK4RBXGdORrj_OtCZK1o6UKdaWZU-gO1yic'));
 
     return result;
 }
