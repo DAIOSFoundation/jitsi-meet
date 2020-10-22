c5878e4406969849ae26d6f57586d3da706c612f
서강혁 <rkdgur7007@gmail.com>
2020-10-22 11:32:55 +0900

    [수정] webpack.config.js 사이즈 조정

--- webpack.config.js
+++ webpack.config.js
@@ -187,7 +187,7 @@ module.exports = [
         entry: {
             'app.bundle': './app.js'
         },
-        performance: getPerformanceHints(4 * 1024 * 1024)
+        performance: getPerformanceHints(7 * 1024 * 1024)
     }),
     Object.assign({}, config, {
         entry: {
