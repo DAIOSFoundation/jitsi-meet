fdd7255e60ad378d62361a03efe8b190bc205bca
서강혁 <rkdgur7007@gmail.com>
2020-11-13 16:49:00 +0900

    code refactoring

--- react/modules/rootSaga.js
+++ react/modules/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import uploadSaga from "./upload/sagas";
 import authSaga from "./auth/sagas";
+import oauthSaga from "./oauth/sagas";
 
 export default function* rootSaga() {
     yield all([
         uploadSaga(),
-        authSaga()
+        authSaga(),
+        oauthSaga(),
     ])
 }
