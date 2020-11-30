8650be247de5f1223b30b8fdb101afcd605921d5
서강혁 <rkdgur7007@gmail.com>
2020-11-30 18:01:58 +0900

    setting icon test

--- react/features/base/icons/components/Icon.js
+++ react/features/base/icons/components/Icon.js
@@ -75,7 +75,7 @@ export default function Icon(props: Props) {
     return (
         <div>
             {
-                (props.src.name === 'SvgSettings' || props.src.name === 'cr') ?
+                (props.src.name === 'SvgSettings' || props.src.name === 'qn') ?
                     <Container
                         onClick={onClick}
                         style={restStyle}>
